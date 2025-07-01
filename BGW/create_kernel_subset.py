03b4ffcb80932cd5548b36bedb0885b357d4b301
Rafael Del Grande <rafaeldgrande@gmail.com>
2025-07-01 11:29:52 -0700

    fixed some index bugs

--- BGW/create_kernel_subset.py
+++ BGW/create_kernel_subset.py
@@ -5,34 +5,44 @@ import argparse
 # author: RRDG
 # date: 2025/2/27
 
-''' This script modifies the number of valence and conduction bands in a BSE kernel file.
-    It reads  the original file bsemat.h5 and creates a new file bsemat_mod.h5 with the 
-    modified number of bands. Assuming restricted kernel
-    
-    Usage:
-    python create_kernel_subset.py nc_new nv_new
-    where nc_new and nv_new are the new number of conduction and valence bands, respectively.
+'''
+This script modifies the number of valence and conduction bands in a BSE kernel file.
+It reads the original file (default: bsemat.h5) and creates a new file (default: bsemat_mod.h5)
+with the modified number of bands. Assuming restricted kernel.
+
+Usage:
+    python create_kernel_subset.py --nc <new_conduction_bands> --nv <new_valence_bands> \
+                                   --input bsemat.h5 --output bsemat_mod.h5
 '''
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Modify bsemat.h5 by reducing conduction and valence bands.")
-parser.add_argument("mv", type=int, help="New number of valence bands")
-parser.add_argument("mc", type=int, help="New number of conduction bands")
+parser.add_argument("--nv", type=int, required=True, help="New number of valence bands")
+parser.add_argument("--nc", type=int, required=True, help="New number of conduction bands")
+parser.add_argument("--input", type=str, default="bsemat.h5", help="Input kernel file (default: bsemat.h5)")
+parser.add_argument("--output", type=str, default="bsemat_mod.h5", help="Output kernel file (default: bsemat_mod.h5)")
 args = parser.parse_args()
+
+nv_new = args.nv
+nc_new = args.nc
+input_file = args.input
+output_file = args.output
 
 # Open the original file
 with h5py.File("bsemat.h5", "r") as f_orig:
     # Read original number of valence and conduction bands
-    nv = f_orig["/bse_header/bands/nvb"][()]
-    nc = f_orig["/bse_header/bands/ncb"][()]
+    nv_orig = f_orig["/bse_header/bands/nvb"][()]
+    nc_orig = f_orig["/bse_header/bands/ncb"][()]
     
-    if nc < args.mc:
-        raise ValueError(f"New number of conduction bands ({args.mc}) is greater than the original number ({nc}).")
-    if nv < args.mv:
-        raise ValueError(f"New number of valence bands ({args.mv}) is greater than the original number ({nv}).")
+    print(f"Original kernel: nv = {nv_orig}, nc = {nc_orig}")
+    print(f"New kernel: nv = {nv_new}, nc = {nc_new}")
+    
+    if nc_orig < nc_new:
+        raise ValueError(f"New number of conduction bands ({nc_new}) is greater than the original number ({nc_orig}).")
+    if nv_orig < nv_new:
+        raise ValueError(f"New number of valence bands ({nv_new}) is greater than the original number ({nv_orig}).")
 
-    print(f"Original kernel: nv = {nv}, nc = {nc}")
-    print(f"New kernel: nv = {args.mv}, nc = {args.mc}")
+
 
     # Create a new file
     with h5py.File("bsemat_mod.h5", "w") as f_new:
@@ -41,10 +51,10 @@ with h5py.File("bsemat.h5", "r") as f_orig:
             f_orig.copy(name, f_new)
 
         # Modify specific datasets
-        f_new["/bse_header/bands/nvb"][()] = args.mv
-        f_new["/bse_header/bands/ncb"][()] = args.mc
-        f_new["/bse_header/bands/n1b"][()] = args.mv  # Assuming restricted kernel
-        f_new["/bse_header/bands/n2b"][()] = args.mc  # Assuming restricted kernel
+        f_new["/bse_header/bands/nvb"][()] = nv_new
+        f_new["/bse_header/bands/ncb"][()] = nc_new
+        f_new["/bse_header/bands/n1b"][()] = nv_new  # Assuming restricted kernel
+        f_new["/bse_header/bands/n2b"][()] = nc_new  # Assuming restricted kernel
 
         # Modify the kernel matrix datasets
         for mat_name in ["head", "wing", "body", "exchange", "fxc"]:
@@ -52,17 +62,17 @@ with h5py.File("bsemat.h5", "r") as f_orig:
             if mat_path in f_new:
                 data = f_new[mat_path][:]
                 
-                
-                # nk*ns, nk*ns, nc, nc, nv, nv, flavor
+                # shape of data
+                # nk*ns, nk*ns, nc_orig, nc_orig, nv_orig, nv_orig, flavor
                 # nk = number of k-points, ns = spin components
                 # flavo = 1 (real) or 2 (complex)
 
                 # Determine new shape
                 new_shape = list(data.shape)
-                new_shape[2] = args.mv  # Modify valence dimension
-                new_shape[3] = args.mv  # Modify valence dimension
-                new_shape[4] = args.mc  # Modify conduction dimension
-                new_shape[5] = args.mc  # Modify conduction dimension
+                new_shape[2] = nc_new  # Modify valence dimension
+                new_shape[3] = nc_new  # Modify valence dimension
+                new_shape[4] = nv_new  # Modify conduction dimension
+                new_shape[5] = nv_new  # Modify conduction dimension
                 
                 if mat_name == 'head':
                     print(f'Previous shape {data.shape}')
@@ -73,7 +83,7 @@ with h5py.File("bsemat.h5", "r") as f_orig:
                 f_new.create_dataset(mat_path, shape=tuple(new_shape), dtype=data.dtype)
 
                 # Copy relevant data
-                f_new[mat_path][:] = data[:, :, :args.mv, :args.mv, :args.mc, :args.mc, :]
+                f_new[mat_path][:] = data[:, :, :nc_new, :nc_new, :nv_new, :nv_new, :]
                 
                 print('Finished copying', mat_name)
 
